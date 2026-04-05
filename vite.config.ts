ea7056a5d877a55fba13c14a030f568553f936e6
Elie Lambeaux <elie.lambeaux@outlook.fr>
2026-04-05 14:41:19 +0200

    Configuration déploiement automatique

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/cabinet-psychomot-web-oriane/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

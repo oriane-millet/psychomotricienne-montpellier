5f0d6de8fee5a2a296988a9a8321c2bfc8305580
Oriane Millet <oriane.millet77@gmail.com>
2026-04-19 17:24:21 +0200

    Correction pour déploiment Hostinger

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/psychomotricienne-montpellier/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
